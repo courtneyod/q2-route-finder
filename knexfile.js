fa29df2c5e1d8900ccf51211ba178aa388e76521
Courtney ODonnell <courtney.od@gmail.com>
2016-11-11 17:25:06 -0800

    express generator complete

---
+++ knexfile.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/route_finder_dev',
+    pool: {
+      min: 1,
+      max: 1
+    }
+  },
+
+  test: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/route_finder_test',
+    pool: {
+      min: 1,
+      max: 1
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/route_finder_dev',
+    pool: {
+      min: 1,
+      max: 1
+    }
+  }
+};
